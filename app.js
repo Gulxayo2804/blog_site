9d2ede84f5cb8a93a0f78ee43d260e192c405410
Gulxayo2804 <gulxayo02@mail.ru>
2025-10-29 19:59:13 -0400

    connecting with db

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const moongose = require('mongoose');
+const app = express();
+
+
+const MONGODB_URI =
+    'mongodb+srv://gulkhayo:gulkhayo@cluster0.rkdaucr.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0';
+
+moongose
+    .connect(MONGODB_URI)
+    .then(() => {
+        app.listen(8080, () => {
+            console.log('Working..')
+        })
+    })
+    .catch(err => { console.log('error connection with db') })
+
