5fdc594baa1c366c9655f0bfba82713a36c360e4
Gulxayo2804 <gulxayo02@mail.ru>
2025-10-29 20:15:38 -0400

    creating User and Post models

---
+++ model/Post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const postModel = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    creator: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Post', postModel)
--- model/User.js
+++ model/User.js
@@ -1,2 +1,29 @@
 const mongoose = require('mongoose');
 
+const UserModel = mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    status:{
+        type:String,
+        default:'I am new!'
+    },
+    posts: [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Post'
+    }]
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.Model("User", UserModel);
