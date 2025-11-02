22b4e43f119e08ae3b51a5f856fb362e1046bf51
Gulxayo2804 <gulxayo02@mail.ru>
2025-11-02 16:15:59 -0500

    fix corse problems

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.use(express.json());
 const MONGODB_URI =
     'mongodb+srv://gulkhayo:gulkhayo@cluster0.rkdaucr.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0';
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/auth', require('./routes/auth'));
 // app.use('/feed', require('./routes/feed'));
 
